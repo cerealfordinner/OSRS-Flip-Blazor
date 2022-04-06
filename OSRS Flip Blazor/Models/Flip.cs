be5ec0828de9dbda71d54754e97f1297917f4808
cerealfordinner <cerealfordinnerx@gmail.com>
2022-04-06 18:51:29 -0400

    Add project files.

---
+++ OSRS Flip Blazor/Models/Flip.cs
@@ -0,0 +1,22 @@
+﻿namespace OSRS_Flip_Blazor.Models
+{
+    public class Flip
+    {
+        public string ItemName { get; set; }
+        public int BuyPrice { get; set; }
+        public int SellPrice { get; set; }
+        public int Quantity { get; set; }
+        public int Margin { get; set; }
+        public int Profit { get; set; }
+
+        public Flip(string itemName, int buyPrice, int sellPrice, int quantity)
+        {
+            ItemName = itemName;
+            BuyPrice = buyPrice;
+            SellPrice = sellPrice;
+            Quantity = quantity;
+            Margin = sellPrice - buyPrice;
+            Profit = Margin * quantity;
+        }
+    }
+}
