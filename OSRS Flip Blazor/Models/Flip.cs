55928ab720529771e8095c923875390e855078ec
cerealfordinner <cerealfordinnerx@gmail.com>
2022-04-08 20:11:45 -0400

    Fetch

--- OSRS Flip Blazor/Models/Flip.cs
+++ OSRS Flip Blazor/Models/Flip.cs
@@ -13,16 +13,6 @@ namespace OSRS_Flip_Blazor.Models
         public int Quantity { get; set; }
         public int Margin { get; set; }
         public int Profit { get; set; }
-
-        public Flip(string itemName, int buyPrice, int sellPrice, int quantity)
-        {
-            ItemName = itemName;
-            BuyPrice = buyPrice;
-            SellPrice = sellPrice;
-            Quantity = quantity;
-            Margin = sellPrice - buyPrice;
-            Profit = Margin * quantity;
-        }
     }
     public class IndexModel : PageModel
     {
